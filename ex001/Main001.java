3eed2557e21d9f7c64bf3c2a3e9b38ebc70ac4c2
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-07-08 18:30:20 -0300

    Update Main001.java

--- ex001/Main001.java
+++ ex001/Main001.java
@@ -1,3 +1,24 @@
 /*
 Faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo
 */
+
+import java.util.Scanner;
+
+public class Main001
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe um numero:  ");
+        int num = scanner.nextInt();
+        
+        
+        if(num < 0){
+            System.out.println("O numero eh negativo");
+        }else if (num == 0){
+            System.out.println("O numero eh neutro");
+        }else{
+            System.out.println("O numero eh positivo");
+        }
+	}
+}
