f1771255f97dfcb2efc72948f98f2afcf917f055
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-07-08 18:24:13 -0300

    Done Exercise 000

--- ex000/Main000.java
+++ ex000/Main000.java
@@ -1,3 +1,24 @@
 /*
 Faça um programa que peça dois números e imprima o maior deles.
 */
+
+import java.util.Scanner;
+
+public class Main000
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe um numero:  ");
+        int num1 = scanner.nextInt();
+        
+        System.out.println("Informe outro numero:  ");
+        int num2 = scanner.nextInt();
+        
+        if(num1 > num2){
+            System.out.println("O maior numero eh o " + num1);
+        }else{
+            System.out.println("O maior numero eh o " + num2);
+        }
+	}
+}
