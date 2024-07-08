788289b4009c077dbf52e0c30760d39410f9874b
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-07-08 18:36:19 -0300

    Done Exercise 002

--- ex002/Main002.java
+++ ex002/Main002.java
@@ -1,3 +1,22 @@
 /*
 Faça um programa que peça um número e imprima se o número é par ou ímpar
 */
+
+import java.util.Scanner;
+
+public class Main001
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe um numero:  ");
+        int num = scanner.nextInt();
+        
+        
+        if(num % 2 == 0){
+            System.out.println("O numero eh par");
+        }else{
+            System.out.println("O numero eh impar");
+        }
+	}
+}
