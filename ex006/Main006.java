f6cc06be57b2d83c62b5830e0dd728455b8f2493
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-07-18 10:17:00 -0300

    Done Exercise 6

--- ex006/Main006.java
+++ ex006/Main006.java
@@ -1,3 +1,27 @@
 /*
 Faça um programa que verifique se uma letra digitada é vogal ou consoante.
 */
+
+import java.util.Scanner;
+
+public class Main006
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite uma letra");
+		char letra = scanner.next().charAt(0);
+		
+		if(
+		   letra == 'a' || letra == 'A'||
+		   letra == 'e' || letra == 'E'||
+		   letra == 'i' || letra == 'I'||
+		   letra == 'o' || letra == 'O'||
+		   letra == 'u' || letra == 'U'
+		 ){
+		    System.out.println("A letra digitada eh uma vogal");
+		}else{
+		    System.out.println("A letra digitada eh uma consoante");
+		}
+	}
+}
