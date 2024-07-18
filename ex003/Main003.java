2429e328b1519c8ff369217a0bd9f34703635669
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-07-18 09:53:17 -0300

    Done exercise 3

--- ex003/Main003.java
+++ ex003/Main003.java
@@ -1,3 +1,23 @@
 /*
 Faça um programa que peça um número e se este número for par, transforme-o em ímpar e vice-versa
 */
+
+import java.util.Scanner;
+
+public class Main003
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite um numero");
+		int num = scanner.nextInt();
+		
+		if(num % 2 == 0){
+		    num++;
+		    System.out.println(num + " o numero eh impar");
+		}else{
+		    num--;
+		    System.out.println(num + " o numero eh par");
+		}
+	}
+}
