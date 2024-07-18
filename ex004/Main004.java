49ad4baa8c38f3dbe1cada1e50c87a09c60d0775
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-07-18 09:56:51 -0300

    Done Exercise 4

--- ex004/Main004.java
+++ ex004/Main004.java
@@ -1,3 +1,21 @@
 /*
 Faça um programa que peça um número e informe se o número é inteiro ou quebrado
 */
+
+import java.util.Scanner;
+
+public class Main004
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite um numero");
+		double num = scanner.nextDouble();
+		
+		if(num == (int)num){
+		    System.out.println("esse numero eh inteiro");
+		}else{
+        System.out.println("esse numero eh quebrado");
+		}
+	}
+}
