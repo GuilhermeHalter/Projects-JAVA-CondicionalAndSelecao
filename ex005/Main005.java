f7000ac028dd21b61316379d3b025c0fd0eedcfe
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-07-18 10:05:50 -0300

    Done Exercise 5

--- ex005/Main005.java
+++ ex005/Main005.java
@@ -1,3 +1,23 @@
 /*
 Faça um programa que verifique se a letra digitada for: F ou M e imprima uma mensagem: masculino, feminino ou sexo inválido.
 */
+
+import java.util.Scanner;
+
+public class Main005
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite o sexo (F ou M)");
+		char letra = scanner.next().charAt(0);
+		
+		if(letra == 'm' || letra == 'M'){
+		    System.out.println("o sexo eh masculino");
+		}else if(letra == 'f' || letra == 'F'){
+        System.out.println("o sexo eh feminino");
+		}else{
+		    System.out.println("sexo invalido");
+		}
+	}
+}
