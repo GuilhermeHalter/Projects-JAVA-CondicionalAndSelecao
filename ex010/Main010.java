323dca2faa8586124cfb27200cb6f70a53cca731
Guilherme Halter Nunes <99202829+GuilhermeHalter@users.noreply.github.com>
2024-07-22 17:17:45 -0300

    Done Exercise 10

--- ex010/Main010.java
+++ ex010/Main010.java
@@ -7,3 +7,54 @@ O menu será como segue:
 [ 4 ] – Divisão
 [ 5 ] – Fim
 */
+
+import java.util.Scanner;
+
+public class Main010
+{
+	public static void main(String[] args) {
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    
+	    System.out.println("[ 1 ] – Adição");
+	    System.out.println("[ 2 ] – Subtração");
+	    System.out.println("[ 3 ] – Multiplicação");
+	    System.out.println("[ 4 ] – Divisão");
+	    System.out.println("[ 5 ] – Fim");
+
+        
+        System.out.println("Digite uma opcao");
+		int opcao = scanner.nextInt();
+	    
+		System.out.println("Digite um numero");
+		int num1 = scanner.nextInt();
+		
+		System.out.println("Digite outro numero");
+		int num2 = scanner.nextInt();
+		
+         switch(opcao){
+            case 1:
+                System.out.println(num1 + num2);
+                break;
+            case 2:
+                System.out.println(num1 - num2);
+                break;
+            case 3:
+                System.out.println(num1 * num2);
+                break;
+            case 4:
+                if(num2 == 0){
+                    System.out.println("nao eh possivel dividir um numero por 0; digite outro numero");
+                }else{
+                    System.out.println(num1 / num2);
+                }
+                break;
+            case 5:
+                break;
+            default:
+                    System.out.println("numero invalido");
+        }
+		
+
+	}
+}
